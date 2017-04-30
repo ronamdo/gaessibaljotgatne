d0c8bbd1a878e5411a10a8c859092da5393fbb84
ronamdo <onerndj2@gmail.com>
2017-05-01 00:18:29 +0900

    ㄴㅇㄹㄴㄹㄷㄹㅈㄷ

---
+++ hello.py
@@ -0,0 +1,9 @@
+// For command line git tutorial
+// First: GitHub modification
+// Second: Local repository modification
+
+print("Hello world")
+print("Tell Your World")
+print("Tell his world")
+print("Tell Her World")
+print("Tell my world")
